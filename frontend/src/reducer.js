98ffa197890d7388df5dfbc0c5dfdce1f67b2637
danil321654 <danil321654@gmail.com>
2021-02-20 23:10:02 +0300

    Delete frontend directory

--- frontend/src/reducer.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from "redux";
-import {authReducer} from "./auth/reducers/authReducer";
-import {notesReducer} from "./notes/reducers/notesReducer";
-
-export default combineReducers({auth: authReducer, app: notesReducer});
-  
