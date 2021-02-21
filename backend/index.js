a7c111df7b4dabbd39214d6cc0977e02eb0c34c6
danil321654 <danil321654@gmail.com>
2021-02-21 13:41:04 +0300

    optimize for deployment

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
 const passportConfig = require("./config/passport-config");
+const path = require("path");
 const db = require("./config/db");
 const routes = require("./routes/index");
 
@@ -13,6 +14,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+}
+
 app.use(routes);
+
+app.use("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"))
+);
 
 app.listen(3004);
